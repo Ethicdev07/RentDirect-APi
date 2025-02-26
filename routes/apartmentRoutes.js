ffad359ce597bc584090b049afdd4b76b1940022
Ethicdev07 <akinadejunior76@gmail.com>
2025-02-26 08:05:19 +0100

    files updated

--- routes/apartmentRoutes.js
+++ routes/apartmentRoutes.js
@@ -11,30 +11,22 @@ const {
   deleteApartment,
 } = require("../controllers/apartmentcontroller");
 
-router.post("/", protect, async (req, res) => {
-  imageUploads(req, res, async (err) => {
-    try {
-      if (err) {
-        return res.status(400).json({ error: `Upload error: ${err.message}` });
-      }
-
-      if (!req.files || req.files.length < 5) {
-        return res
-          .status(400)
-          .json({ error: "Please upload at least 5 images" });
-      }
-
-      req.body.images = req.files.map((file) => file.path);
-
-      await createApartment(req, res);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: error.message || "Error creating apartment" });
+router.post("/", protect, imageUploads, async (req, res) => {
+  try {
+    if (!req.files || req.files.length < 5) {
+      return res.status(400).json({ error: "Please upload at least 5 images" });
     }
-  });
-});
 
+    req.body.images = req.files.map((file) => file.path);
+
+    await createApartment(req, res);
+    
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message || "Error creating apartment" });
+  }
+});
 
 router.get("/search", searchApartments);
 router.get("/:id", getOne);
